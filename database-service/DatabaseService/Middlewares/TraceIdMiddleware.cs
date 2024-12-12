76994956ed28f8113f9eec792e259d024bfd6249
nikit <nikita.milanov2@mail.ru>
2024-12-13 01:17:31 +0500

    added tracing

---
+++ database-service/DatabaseService/Middlewares/TraceIdMiddleware.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace DatabaseService.Middlewares;
+
+public class TraceIdMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var traceId = Activity.Current?.TraceId.ToString() ?? "no-trace-id";
+        context.Response.Headers.Append("X-Trace-Id", traceId);
+        await next(context);
+    }
+}
